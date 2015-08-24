9721ceb5fd6bfce4216df5fbc5f7139667036ca3
juliodieguez8a <juliodieguez8a@gmail.com>
2015-08-24 17:13:48 -0600

    final final final XD

--- HojaTrabajo05.py
+++ HojaTrabajo05.py
@@ -13,17 +13,20 @@ import random
 
 def proceso(env, t_crea, nombre, ram, mem, ins, ins_x_t):
 
+    global tiempoTOTAL #variable con el tiempo TOTAL acumulado de los procesos
+    
     #NEW
     # Simular tiempo de llegada de proceso
     yield env.timeout(t_crea)
     print('t: %f - %s (new) solicita -> %d de ram' % (env.now, nombre, mem))
 
+    #Obtener tiempo de llegada
+    tiempoLlegada = env.now # aqui llego el proceso
+    
     #ADMITTED->READY
     #solicita memoria
     yield ram.get(mem)
     print('t: %f - %s (admited) acepta solicitud por -> %d de ram' % (env.now, nombre, mem))
-
-    
 
     
     completado = 0
@@ -69,12 +72,20 @@ def proceso(env, t_crea, nombre, ram, mem, ins, ins_x_t):
     yield ram.put(mem)
     print('t: %f - %s (terminated) finaliza, retorna -> %d de ram' % (env.now, nombre, mem))
 
+    #Actualizar tiempo Total
+    # Se agraga al tiempo total el tiempo de este proceso (tiempo actual - tiempo de llegada)
+    tiempoTOTAL += (env.now - tiempoLlegada)  
 
 
-    
+
+memoria_ram=100 #100 unidades de memoria ram
+ins_x_t = 3.0 #ejecuta 3 instrucciones por unidad de tiempo
+tiempoTOTAL = 0.0 #tiempo de todos los procesos
+n_procesos = 25 # numero de procesos a ejecutar
+
 
 env = simpy.Environment()  #crear ambiente de simulacion
-ram = simpy.Container(env, init=100, capacity=100) #el cargador de bateria soporta 2 carros
+ram = simpy.Container(env, init=memoria_ram, capacity=memoria_ram) #el cargador de bateria soporta 2 carros
 cpu = simpy.Resource (env, capacity=1) #cola para acceso a cpu
 waiting = simpy.Resource (env, capacity=1) #cola para acceso a operaciones i/o
 
@@ -82,10 +93,10 @@ waiting = simpy.Resource (env, capacity=1) #cola para acceso a operaciones i/o
 random.seed(1234)
 
 interval = 10 #cada 10
-ins_x_t = 3.0 #ejecuta 3 instrucciones por unidad de tiempo
+
 
 # crear los procesos
-for i in range(25):
+for i in range(n_procesos):
     t_crea = random.expovariate(1.0 / interval)
     ins = random.randint(1,10) #Cantidad de instrucciones
     mem = random.randint(1,10) #Cantidad de memoria a solicitar
@@ -93,4 +104,6 @@ for i in range(25):
 
 # correr la simulacion
 env.run()
+print "El PROMEDIO de tiempo por proceso es ", tiempoTOTAL / n_procesos
 print "fin"
+
